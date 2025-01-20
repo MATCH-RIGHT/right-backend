863427ce6fdc78142e66da9bea1ab21807511af9
MongsangGa <cldnwjd1024@gmail.com>
2025-01-20 16:43:43 +0900

    git & intellij settings
    - add PR & ISSUE TEMPLATE
    - add gitignore
    - add dependencies

---
+++ src/main/java/com/example/rightbackend/RightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rightbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RightBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rightbackend/RightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rightbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
