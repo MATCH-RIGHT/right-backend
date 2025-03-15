0679ffe4766599a8d0410d5c6502fcfe80ed06e9
MongsangGa <cldnwjd1024@gmail.com>
2025-03-16 01:11:48 +0900

    refactor(MemberController): ResponseEntity 추가

--- src/main/java/com/example/rightbackend/member/controller/MemberController.java
+++ src/main/java/com/example/rightbackend/member/controller/MemberController.java
@@ -1,9 +1,17 @@
 package com.example.rightbackend.member.controller;
 
+import com.example.rightbackend.auth.controller.dto.LoginMember;
+import com.example.rightbackend.auth.domain.Member;
+import com.example.rightbackend.global.config.resolver.Login;
 import com.example.rightbackend.global.response.SuccessResponse;
 import com.example.rightbackend.global.response.success.MemberSuccess;
+import com.example.rightbackend.member.controller.dto.request.CheckIdRequest;
+import com.example.rightbackend.member.controller.dto.request.ResetPasswordRequest;
+import com.example.rightbackend.member.controller.dto.request.SearchIdRequest;
 import com.example.rightbackend.member.controller.dto.request.SignUpRequest;
+import com.example.rightbackend.member.controller.dto.response.MemberPageResponse;
 import com.example.rightbackend.member.service.MemberProfileService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/member")
@@ -16,7 +24,29 @@ public class MemberController {
     }
 
     @PostMapping("/sign-up")
-    public SuccessResponse<String> signUp(@RequestBody SignUpRequest request) {
-        return SuccessResponse.of(MemberSuccess.SING_UP_SUCCESS, memberProfileService.signUp(request));
+    public ResponseEntity<SuccessResponse<String>> signUp(@RequestBody SignUpRequest request) {
+        return SuccessResponse.of(MemberSuccess.SIGN_UP_SUCCESS, memberProfileService.signUp(request));
+    }
+
+    @PostMapping("/check-id")
+    public ResponseEntity<SuccessResponse<String>> checkId(@RequestBody CheckIdRequest givenId) {
+        return SuccessResponse.of(MemberSuccess.CHECK_ID_SUCCESS, memberProfileService.checkDuplicateId(givenId));
+    }
+
+    @GetMapping("/member-page")
+    public ResponseEntity<SuccessResponse<MemberPageResponse>> getMemberPage(@Login LoginMember loginMember) {
+        return SuccessResponse.of(
+                MemberSuccess.GET_MEMBER_PAGE_SUCCESS,
+                memberProfileService.getMemberPage(loginMember));
+    }
+
+    @PostMapping("/search-id")
+    public ResponseEntity<SuccessResponse<String>> searchId(@RequestBody SearchIdRequest request) {
+        return SuccessResponse.of(MemberSuccess.SEARCH_ID_SUCCESS, memberProfileService.searchId(request));
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<SuccessResponse<String>> resetPassword(@RequestBody ResetPasswordRequest request) {
+        return SuccessResponse.of(MemberSuccess.CHANGE_PASSWORD_SUCCESS, memberProfileService.resetPassword(request));
     }
 }
