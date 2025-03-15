ba23c993181ba0ad00f8b4dc05a8da976cb989fb
MongsangGa <cldnwjd1024@gmail.com>
2025-03-16 00:56:44 +0900

    refactor(MemberRepository): JPA 메소드 추가

--- src/main/java/com/example/rightbackend/auth/domain/repository/MemberRepository.java
+++ src/main/java/com/example/rightbackend/auth/domain/repository/MemberRepository.java
@@ -4,6 +4,12 @@ import com.example.rightbackend.auth.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByProviderId(String providerId);
+    Optional<Member> findById(Long id);
+    Optional<Member> findFirstByNameAndPhoneNumber(String name, String phoneNumber);
 }
