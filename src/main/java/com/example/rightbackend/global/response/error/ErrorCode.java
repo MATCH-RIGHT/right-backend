3a83826b8a9d5d9ec643cd1982088289ecf27324
MongsangGa <cldnwjd1024@gmail.com>
2025-03-16 01:03:18 +0900

    feat(ErrorCode): 에러코드 인터페이스 추가

---
+++ src/main/java/com/example/rightbackend/global/response/error/ErrorCode.java
@@ -0,0 +1,9 @@
+package com.example.rightbackend.global.response.error;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    HttpStatus getHttpStatus();
+    String getCode();
+    String getMessage();
+}
