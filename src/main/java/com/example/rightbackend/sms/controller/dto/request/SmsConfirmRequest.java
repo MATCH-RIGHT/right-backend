2bd6fe2fb3ac0b8f06eeee90ef709b85d39d1fdc
MongsangGa <cldnwjd1024@gmail.com>
2025-03-16 01:15:57 +0900

    feat(SmsConfirmRequest, SmsSendRequest): 인증번호 전송 및 검사 요청 객체 추가

---
+++ src/main/java/com/example/rightbackend/sms/controller/dto/request/SmsConfirmRequest.java
@@ -0,0 +1,4 @@
+package com.example.rightbackend.sms.controller.dto.request;
+
+public record SmsConfirmRequest(String phoneNumber, String certificationNumber){
+}
---
+++ src/main/java/com/example/rightbackend/sms/controller/dto/request/SmsSendRequest.java
@@ -0,0 +1,4 @@
+package com.example.rightbackend.sms.controller.dto.request;
+
+public record SmsSendRequest(String phoneNumber){
+}
