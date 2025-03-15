df5fa8eb09e5ebf333bc16daf22bbdb925b75f2b
MongsangGa <cldnwjd1024@gmail.com>
2025-03-16 01:06:15 +0900

    feat(AuthSuccess): Auth 도메인에서 처리하는 성공코드 추가

---
+++ src/main/java/com/example/rightbackend/global/response/success/AuthSuccess.java
@@ -0,0 +1,37 @@
+package com.example.rightbackend.global.response.success;
+
+import org.springframework.http.HttpStatus;
+
+public enum AuthSuccess implements SuccessCode {
+    GENERATE_TOKEN_SUCCESS(HttpStatus.OK, "A001", "토큰 발급에 성공했습니다"),
+    LOGOUT_SUCCESS(HttpStatus.OK, "A002", "로그아웃에 성공했습니다"),
+    REISSUE_TOKEN_SUCCESS(HttpStatus.OK, "A003", "토큰 재발급에 성공했습니다"),
+    WITH_DRAW_SUCCESS(HttpStatus.OK, "A004", "회원 탈퇴가 완료되었습니다"),
+    GET_PASSWORD_SUCCESS(HttpStatus.OK, "A005", "비밀번호 조회가 완료되었습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    AuthSuccess(final HttpStatus httpStatus, final String code, final String message) {
+        this.httpStatus = httpStatus;
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
