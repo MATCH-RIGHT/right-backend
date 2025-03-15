a2199fe17546fd3c0736b059d827ac9650f65557
MongsangGa <cldnwjd1024@gmail.com>
2025-03-16 01:01:07 +0900

    feat(TokenResolver): 토큰을 디코딩해 LoginMember로 바인딩하는 리졸버 추가

---
+++ src/main/java/com/example/rightbackend/global/config/resolver/TokenResolver.java
@@ -0,0 +1,31 @@
+package com.example.rightbackend.global.config.resolver;
+
+import com.example.rightbackend.auth.controller.dto.LoginMember;
+import com.example.rightbackend.auth.service.TokenProvider;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class TokenResolver implements HandlerMethodArgumentResolver {
+
+    private final TokenProvider tokenProvider;
+
+    public TokenResolver(final TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public boolean supportsParameter(final MethodParameter parameter) {
+        return parameter.getParameterType().isAssignableFrom(LoginMember.class) && parameter.hasParameterAnnotation(Login.class);
+    }
+
+    @Override
+    public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer, final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
+        String Token = webRequest.getHeader("Authorization");
+        return tokenProvider.getLoginFromToken(Token);
+    }
+}
