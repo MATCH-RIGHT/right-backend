fad282c11253b791cfa6c7a73bffc0382c904d5b
MongsangGa <cldnwjd1024@gmail.com>
2025-03-16 00:59:09 +0900

    feat(TokenProvider): 토큰 생성 및 검증 클래스 추가

---
+++ src/main/java/com/example/rightbackend/auth/service/TokenProvider.java
@@ -0,0 +1,112 @@
+package com.example.rightbackend.auth.service;
+
+import com.example.rightbackend.auth.controller.dto.LoginMember;
+import com.example.rightbackend.auth.domain.MemberRole;
+import com.example.rightbackend.global.exception.RestApiException;
+import com.example.rightbackend.global.properties.JWTProperties;
+import com.example.rightbackend.global.response.error.TokenError;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class TokenProvider {
+
+    private JWTProperties jwtProperties;
+
+    public TokenProvider(JWTProperties jwtProperties) {
+        this.jwtProperties = jwtProperties;
+    }
+
+    // AccessToken
+    public String generateAccessToken(final LoginMember loginMember) {
+        Claims claims = getClaimsFrom(loginMember);
+        return getTokenFrom(claims, jwtProperties.getAccessTokenValidTime());
+    }
+
+    private Claims getClaimsFrom(final LoginMember loginMember) {
+        Claims claims = Jwts.claims();
+        claims.put("memberId", loginMember.memberId());
+        claims.put("role", loginMember.role().getCode());
+        return claims;
+    }
+
+    private String getTokenFrom(final Claims claims, final Long TokenValidTime) {
+        Date now = new Date();
+        return Jwts.builder()
+                .setHeaderParam("type", "JWT")
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + TokenValidTime))
+                .signWith(Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // RefreshToken
+    public String generateRefreshToken(final LoginMember loginMember, final Long tokenId) {
+        Claims claims = getClaimsFrom(loginMember, tokenId);
+        return getTokenFrom(claims, jwtProperties.getRefreshTokenValidTime());
+    }
+
+    private Claims getClaimsFrom(final LoginMember loginMember, final Long tokenId) {
+        Claims claims = Jwts.claims();
+        claims.put("memberId", loginMember.memberId());
+        claims.put("role", loginMember.role().getCode());
+        claims.put("tokenId", tokenId);
+        return claims;
+    }
+
+
+    public LoginMember getLoginFromToken(final String token) {
+        try{
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes()))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return new LoginMember(Long.parseLong(String.valueOf(claims.get("memberId"))), MemberRole.of((Integer)claims.get("role")));
+        } catch (ExpiredJwtException e){
+            throw new RestApiException(TokenError.EXPIRED_ACCESS_TOKEN);
+        } catch (Exception e){
+            throw new RestApiException(TokenError.INVALID_TOKEN);
+        }
+    }
+
+    private String removeTokenPrefix(final String token) {
+        if (token.startsWith("Bearer ")) {
+            return token.substring(7);
+        }
+        return token;
+    }
+
+    public boolean isNotExpiredToken(final String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes()))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getExpiration()
+                    .before(new Date());
+        } catch (ExpiredJwtException e) {
+            return false;
+        }
+    }
+
+    public Long getTokenIdFromRefreshToken(final String refreshToken) {
+        try {
+            Claims claims = Jwts.parserBuilder().
+                    setSigningKey(Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes())).
+                    build().
+                    parseClaimsJws(removeTokenPrefix(refreshToken)).
+                    getBody();
+            return Long.parseLong(String.valueOf(claims.get("tokenId")));
+        } catch (ExpiredJwtException e) {
+          throw new RestApiException(TokenError.EXPIRED_REFRESH_TOKEN);
+        } catch (Exception e) {
+            throw new RestApiException(TokenError.INVALID_TOKEN);
+        }
+    }
+}
