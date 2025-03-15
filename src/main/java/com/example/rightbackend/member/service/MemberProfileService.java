a4eda2ec5f6d6589f6c7ceb65a79f3a757255ca4
MongsangGa <cldnwjd1024@gmail.com>
2025-03-16 01:15:21 +0900

    refactor(MemberProfileService): 인코딩 추가, 아이디 중복 검사, 기존 회원 검사, 비밀번호 재설정, 멤버 및 토큰 정보 로직 추가

--- src/main/java/com/example/rightbackend/member/service/MemberProfileService.java
+++ src/main/java/com/example/rightbackend/member/service/MemberProfileService.java
@@ -1,25 +1,47 @@
 package com.example.rightbackend.member.service;
 
+import com.example.rightbackend.auth.controller.dto.LoginMember;
 import com.example.rightbackend.auth.domain.Member;
+import com.example.rightbackend.auth.domain.Token;
 import com.example.rightbackend.auth.domain.repository.MemberRepository;
+import com.example.rightbackend.auth.domain.repository.TokenRepository;
+import com.example.rightbackend.auth.service.PasswordEncoder;
+import com.example.rightbackend.global.exception.RestApiException;
+import com.example.rightbackend.global.response.error.MemberError;
+import com.example.rightbackend.global.response.error.TokenError;
 import com.example.rightbackend.member.controller.dto.EncodeMember;
 import com.example.rightbackend.member.controller.dto.EncodeMemberProfile;
+import com.example.rightbackend.member.controller.dto.request.CheckIdRequest;
+import com.example.rightbackend.member.controller.dto.request.ResetPasswordRequest;
+import com.example.rightbackend.member.controller.dto.request.SearchIdRequest;
 import com.example.rightbackend.member.controller.dto.request.SignUpRequest;
+import com.example.rightbackend.member.controller.dto.response.MemberPageResponse;
 import com.example.rightbackend.member.controller.dto.response.MemberResponse;
+import com.example.rightbackend.member.domain.Interest;
 import com.example.rightbackend.member.domain.MemberProfile;
+import com.example.rightbackend.member.domain.MemberProfileToInterest;
+import com.example.rightbackend.member.domain.repository.InterestRepository;
 import com.example.rightbackend.member.domain.repository.MemberProfileRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class MemberProfileService {
 
+    private final PasswordEncoder passwordEncoder;
+    private final TokenRepository tokenRepository;
     private final MemberRepository memberRepository;
     private final MemberProfileRepository memberProfileRepository;
+    private final InterestRepository interestRepository;
 
-    public MemberProfileService(MemberRepository memberRepository, final MemberProfileRepository memberProfileRepository) {
+    public MemberProfileService(PasswordEncoder passwordEncoder, TokenRepository tokenRepository, MemberRepository memberRepository, final MemberProfileRepository memberProfileRepository, InterestRepository interestRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.tokenRepository = tokenRepository;
         this.memberRepository = memberRepository;
         this.memberProfileRepository = memberProfileRepository;
+        this.interestRepository = interestRepository;
     }
 
     @Transactional
@@ -29,6 +51,8 @@ public class MemberProfileService {
 
         MemberProfile memberProfile = getMemberProfileEntity(request, member);
 
+        addInterestsToMemberProfile(memberProfile, request.interests());
+
         memberProfile = memberProfileRepository.save(memberProfile);
 
         member.setMemberProfile(memberProfile);
@@ -36,16 +60,28 @@ public class MemberProfileService {
         return MemberResponse.SIGN_UP_SUCCESS.getMessage();
     }
 
-        private Member saveMemberEntity(final SignUpRequest request) {
+    private void addInterestsToMemberProfile(MemberProfile memberProfile, List<String> interests) {
+        if(interests == null || interests.isEmpty()) {
+            return;
+        }
+        for(String interestName: interests) {
+            Interest interest = interestRepository.findByName(interestName).orElseGet(() -> interestRepository.save(Interest.of(interestName)));
+            MemberProfileToInterest link = MemberProfileToInterest.of(memberProfile, interest);
+            memberProfile.getMemberProfileToInterests().add(link);
+        }
+    }
+
+    private Member saveMemberEntity(final SignUpRequest request) {
         EncodeMember encodeMember = encodeForMember(request);
         return memberRepository.save(Member.of(encodeMember));
     }
 
     private EncodeMember encodeForMember(final SignUpRequest request) {
-        return new EncodeMember(request.provider(),
+        return new EncodeMember(request.name(),
+                request.provider(),
                 request.providerId(),
-                request.password(),
-                request.phoneNumber());
+                passwordEncoder.encrypt(request.password()),
+                TextEncoder.encrypt(request.phoneNumber()));
     }
 
     private MemberProfile getMemberProfileEntity(final SignUpRequest request, final Member member) {
@@ -54,15 +90,54 @@ public class MemberProfileService {
     }
 
     private EncodeMemberProfile encodeForMemberProfile(final SignUpRequest request) {
-        return new EncodeMemberProfile(
-                TextEncoder.encrypt(request.nickname()),
+        return new EncodeMemberProfile(TextEncoder.encrypt(request.nickname()),
                 TextEncoder.encrypt(request.gender()),
                 TextEncoder.encrypt(request.birthday()),
                 TextEncoder.encrypt(request.address()),
                 TextEncoder.encrypt(request.height()),
                 TextEncoder.encrypt(request.body_type()),
                 TextEncoder.encrypt(request.job()),
-                TextEncoder.encrypt(request.myself()),
+                request.interests(),
+                request.myself(),
                 TextEncoder.encrypt(request.gender()));
+    }
+
+    @Transactional(readOnly = true)
+    public String checkDuplicateId(final CheckIdRequest providerId) {
+        if(memberRepository.findByProviderId(String.valueOf(providerId)).isPresent()) {
+            throw new RestApiException(MemberError.DUPLICATE_ID);
+        }
+        return MemberResponse.AVAILABLE_ID.getMessage();
+    }
+
+    @Transactional(readOnly = true)
+    public MemberPageResponse getMemberPage(final LoginMember loginMember) {
+        Member member = getMember(loginMember);
+        return member.getMemberPageResopnse();
+    }
+
+    @Transactional
+    public String searchId(final SearchIdRequest searchIdRequest) {
+        Member member = memberRepository.findFirstByNameAndPhoneNumber(searchIdRequest.name(), searchIdRequest.phoneNumber()).orElseThrow(()
+                -> new RestApiException(MemberError.NULL_MEMBER));
+        return member.getProviderId();
+    }
+
+    @Transactional
+    public String resetPassword(final ResetPasswordRequest resetPasswordRequest) {
+        Member member = memberRepository.findFirstByNameAndPhoneNumber(resetPasswordRequest.name(), resetPasswordRequest.phoneNumber()).orElseThrow(()
+                -> new RestApiException(MemberError.NULL_MEMBER));
+        member.setPassword(passwordEncoder.encrypt(resetPasswordRequest.newPassword()));
+        return MemberResponse.PASSWORD_CHANGE_SUCCESS.getMessage();
+    }
+
+    private Member getMember(final LoginMember loginMember) {
+        return memberRepository.findById(loginMember.memberId()).orElseThrow(()
+                -> new RestApiException(MemberError.NULL_MEMBER));
+    }
+
+    private Token getToken(final Member member) {
+        return tokenRepository.findByMember(member).orElseThrow(()
+                -> new RestApiException(TokenError.NULL_REFRESH_TOKEN));
     }
 }
