1c2baf99cfd6daf65375376ef844b41ef49b4a19
MongsangGa <cldnwjd1024@gmail.com>
2025-05-29 23:03:29 +0900

    refactor(MemberError): 비트 인덱스 관련 오류 추가

--- src/main/java/com/example/rightbackend/global/response/error/MemberError.java
+++ src/main/java/com/example/rightbackend/global/response/error/MemberError.java
@@ -9,7 +9,8 @@ public enum MemberError implements ErrorCode {
     INCORRECT_PASSWORD(HttpStatus.SERVICE_UNAVAILABLE, "M4", "비밀번호가 옳지 않음"),
     WITHDRAW_MEMBER(HttpStatus.BAD_REQUEST,"M5" ,"탈퇴한 회원에 접근" ),
     DUPLICATE_ID(HttpStatus.CONFLICT, "M6","이미 존재하는 회원"),
-    NULL_LOCATION(HttpStatus.CONFLICT, "M7","존재하지 않는 지역");
+    NULL_LOCATION(HttpStatus.CONFLICT, "M7","존재하지 않는 지역"),
+    INVALID_FEATURE_INDEX(HttpStatus.BAD_REQUEST, "M8", "특징 인덱스는 0 이상이어야 합니다.");
 
 
     private final HttpStatus httpStatus;
