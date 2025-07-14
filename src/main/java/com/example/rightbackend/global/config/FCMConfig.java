3c977a8c971146f44ccb5fbe2a233e00d8f171bb
MongsangGa <cldnwjd1024@gmail.com>
2025-07-14 22:35:21 +0900

    feat: FCM 설정 파일 추가

---
+++ src/main/java/com/example/rightbackend/global/config/FCMConfig.java
@@ -0,0 +1,41 @@
+package com.example.rightbackend.global.config;
+
+import com.example.rightbackend.global.properties.FCMProperties;
+import com.example.rightbackend.global.response.error.FCMError;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.example.rightbackend.global.exception.RestApiException;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configurable
+public class FCMConfig {
+
+    private final FCMProperties fcmProperties;
+
+    public FCMConfig(FCMProperties fcmProperties) {
+        this.fcmProperties = fcmProperties;
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            InputStream serviceAccount = new ClassPathResource(fcmProperties.getFcmJson()).getInputStream();
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty())
+                FirebaseApp.initializeApp(options);
+
+        } catch (IOException e) {
+            throw new RestApiException(FCMError.FCM_CONFIGURATION_ERROR);
+        }
+    }
+}
