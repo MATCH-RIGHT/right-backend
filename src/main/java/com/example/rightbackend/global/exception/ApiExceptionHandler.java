ee01b04947738cdc5a3dd3f1ad01ab0f52f00845
MongsangGa <cldnwjd1024@gmail.com>
2025-07-19 18:03:52 +0900

    refactor: 반환 형식 JSON 형식으로 변환

--- src/main/java/com/example/rightbackend/global/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/rightbackend/global/exception/ApiExceptionHandler.java
@@ -6,14 +6,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 @Order(value = Integer.MIN_VALUE)
 public class ApiExceptionHandler {
     @ExceptionHandler(value = RestApiException.class)
     public ResponseEntity<Object> apiException(RestApiException apiException) {
         ErrorCode errorCode = apiException.getErrorCode();
+        Map<String, String> errorResponse = new LinkedHashMap<>();
+        errorResponse.put("code", errorCode.getCode());
+        errorResponse.put("message", errorCode.getMessage());
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
-                .body(errorCode.getMessage());
+                .body(errorResponse);
     }
 }
